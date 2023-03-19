81fa7819877befbe7165cf411cdf531276be762a
Krisjanis Adamsons 221REC031 <112925675+Krisjano@users.noreply.github.com>
2023-03-19 17:10:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import heapq
 
-def parallel_processing(n, m, data):
+def parallel_processing(n, m, times):
     
-    output = []
-    pavedieni = [(0, i) for i in range(n)]
-    heapq.heapify(pavedieni)
-    
+    threads = [(0, i) for i in range(n)]
+    heapq.heapify(threads)
+    results = []
     
     for i in range(m):
-        time = data[i]
-        f_time, pavediens = heapq.heappop(pavedieni)
-        if output:
-            s_time = max(f_time, output[-1][1])
-        else:
-            s_time = f_time
-            
-        f_time = s_time + time
-        
-        output.append((pavediens, s_time))
-        heapq.heappush(pavedieni, (f_time, pavediens))
-        
-    return output
+        job_time = times[i]
+        start_time, thread_id = heapq.heappop(threads)
+        results.append((thread_id, start_time))
+        start_time += job_time
+        heapq.heappush(threads, (start_time, thread_id))
+    return results
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    times = list(map(int, input().split()))
+
+    results = parallel_processing(n, m, times)
+
+    for thread_id, start_time in results:
+        print(thread_id, start_time)
