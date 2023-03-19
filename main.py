0f844ace4750271875554b8be2baecd3dd10aab9
Krisjanis Adamsons 221REC031 <112925675+Krisjano@users.noreply.github.com>
2023-03-19 17:00:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # python3
 
+
+
 import heapq
+
+
 
 n, m = map(int, input().split())
 times = list(map(int, input().split()))
@@ -9,11 +13,17 @@ thread_times = [(0, i) for i in range(n)]
 thread_job_assignments = [[] for _ in range(n)]
 
 for i in range(m):
+    
     job_time = times[i]
+    
     start_time, thread_id = heapq.heappop(thread_times)
+    
     thread_job_assignments[thread_id].append(i)
+    
     finish_time = start_time + job_time
+    
     heapq.heappush(thread_times, (finish_time, thread_id))
+    
     
 for i in range(n):
     print(i, end=' ')
